1a451aec34d6e9b56b686d5a882eac3fc265ed0c
Alan Kebert <adkebert@yahoo.com>
2016-08-19 13:08:37 -0400

    added authentication

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,5 +1,6 @@
 class TermsController < ApplicationController
   before_action :set_term, only: [:show, :edit, :update, :destroy]
+  before_filter :authorize!
 
   # GET /terms
   # GET /terms.json
@@ -64,13 +65,13 @@ class TermsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_term
-      @term = Term.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_term
+    @term = Term.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def term_params
-      params.require(:term).permit(:name, :definition, :author, :web_link, :category_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def term_params
+    params.require(:term).permit(:name, :definition, :author, :web_link, :category_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,14 @@ Rails.application.routes.draw do
 
   resources :categories
   resources :terms
+
+  # User
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  # Session
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  get '/logout' => 'session#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
